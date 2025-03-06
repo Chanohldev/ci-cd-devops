63d1436e4002ba6441112e1b8ee0cf4293b408b7
Chano Huaman Landa <chanohl.dev@gmail.com>
2025-03-05 21:39:46 -0500

    feat: unit test

---
+++ tests/test_main.py
@@ -0,0 +1,28 @@
+import main  # Ahora debería encontrar el módulo
+import unittest
+import sys
+from pathlib import Path
+
+# Obtiene el directorio del archivo actual (test_main.py)
+test_dir = Path(__file__).parent
+# Obtiene el directorio padre (tests)
+project_dir = test_dir.parent
+# Agrega el directorio 'src' al sys.path
+src_dir = project_dir / 'src'
+sys.path.insert(0, str(src_dir))
+
+
+class TestMathOperation(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(main.add(1, 2), 3)
+
+        class TestMathOperation(unittest.TestCase):
+            def test_add(self):
+                self.assertEqual(main.add(1, 2), 3)
+                self.assertEqual(main.add(-1, 1), 0)
+                self.assertEqual(main.add(-1, -1), -2)
+                self.assertEqual(main.add(0, 0), 0)
+                self.assertEqual(main.add(100, 200), 300)
+
+        if __name__ == '__main__':
+            unittest.main()
